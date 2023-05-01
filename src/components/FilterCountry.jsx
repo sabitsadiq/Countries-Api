cfd399141cbcf428d2cc2a0a3ba64769f144d0fe
sadiq <sabitsdq@gmail.com>
2023-05-01 12:20:09 +0100

    responsive design

--- src/components/FilterCountry.jsx
+++ src/components/FilterCountry.jsx
@@ -1,21 +1,17 @@
 import React from "react";
-import { useEffect } from "react";
-// import { Axios } from "axios";
 const FilterCountry = ({ onSelect }) => {
   const filterHandler = (e) => {
     const selectRegion = e.target.value;
     onSelect(selectRegion);
   };
-
-  // useEffect(() => {
-  //   onSelect = { getFilterCountry };
-  // }, []);
   return (
     <select onChange={filterHandler} className="filter-container">
       <option>Filter by region: </option>
+      <option value="America">America</option>
       <option value="Africa">Africa</option>
       <option value="Asia">Asia</option>
       <option value="Europe">Europe</option>
+      <option value="Oceanic">Oceanic</option>
     </select>
   );
 };
